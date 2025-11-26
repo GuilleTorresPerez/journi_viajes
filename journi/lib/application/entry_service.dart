42ba6cfbe165ac3690cd8b52bb2e6a6c2442be26
GuilleTorresPerez <guilletorresperez@gmail.com>
2025-11-26 15:40:48 +0100

    feat: add update method to EntryService for modifying existing entries

--- journi/lib/application/entry_service.dart
+++ journi/lib/application/entry_service.dart
@@ -10,10 +10,12 @@ abstract class EntryService {
   Future<Result<Entry?>> getById(String id);
   Future<Result<List<Entry>>> listByTrip(String tripId, {EntryType? type});
   Stream<List<Entry>> watchByTrip(String tripId, {EntryType? type});
+  Future<Result<Entry>> update(UpdateEntryCommand cmd);
 }
 
 class DefaultEntryService implements EntryService {
   final CreateEntryUseCase _createUC;
+  final UpdateEntryUseCase _updateUC;
   final GetEntryByIdUseCase _getUC;
   final DeleteEntryUseCase _deleteUC;
   final ListEntriesUseCase _listUC;
@@ -22,11 +24,13 @@ class DefaultEntryService implements EntryService {
   DefaultEntryService({
     required EntryRepository repo,
     CreateEntryUseCase? createUC,
+    UpdateEntryUseCase? updateUC,
     GetEntryByIdUseCase? getUC,
     DeleteEntryUseCase? deleteUC,
     ListEntriesUseCase? listUC,
     WatchEntriesUseCase? watchUC,
   })  : _createUC = createUC ?? CreateEntryUseCase(repo),
+        _updateUC = updateUC ?? UpdateEntryUseCase(repo),
         _getUC = getUC ?? GetEntryByIdUseCase(repo),
         _deleteUC = deleteUC ?? DeleteEntryUseCase(repo),
         _listUC = listUC ?? ListEntriesUseCase(repo),
@@ -34,6 +38,9 @@ class DefaultEntryService implements EntryService {
 
   @override
   Future<Result<Entry>> create(CreateEntryCommand cmd) => _createUC(cmd);
+
+  @override
+  Future<Result<Entry>> update(UpdateEntryCommand cmd) => _updateUC(cmd);
 
   @override
   Future<Result<Unit>> deleteById(String id) => _deleteUC(id);
